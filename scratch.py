822d69894e238a4dcd4a7b44ea84db8fbdc51e9e
corpulentgowk <stevenlayne2017@u.northwestern.edu>
2016-04-05 20:57:06 -0500

    Now containes recursive version of dfs

--- scratch.py
+++ scratch.py
@@ -28,3 +28,25 @@ L = [0,4,6,12,13,25,27]
 print "binarySearch test case #1: " + str(binarySearch(L,-1) )
 print "binarySearch test case #2: " + str(binarySearch(L,12))
 #print "binarySearch test case #3: " + str(binarySearch(L,25) )
+
+
+def dfsr(tree,elem): #approach essentially uses the input list(tree) as a queue. Children are extracted and appended to the end of the lists.
+    if tree: #Runs until the queue(tree) is empty
+        val = tree.pop(0) #Takes the first element of the queue(tree) out of the list.
+        if type(val) == int: #Checks to see if it is an int or a list
+            print val #prints value before evaluation
+            if val == elem: #checks to see if the value is equal to the sought element
+                return True
+            else:
+                return False or dfsr(tree,elem)
+        else: #its a list
+            print val[0] #prints value before evaluation
+            if val[0] == elem: #Checks to see if the parent node value is equal to the sought element
+                return True
+            val = val[1:] #takes the cdr since the first was already checked
+            val.reverse() #reverses the list to make it easier to prepend it to the queue
+            #effectively prioritizing the children of the node currently visited at so that the search goes deep
+            for i in val: #iterates trhough the children of the parent node
+                tree.insert(0,i) #prepends the childrend to the queue(tree)
+            return False or dfsr(tree,elem)
+    return False
